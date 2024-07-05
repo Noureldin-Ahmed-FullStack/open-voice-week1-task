59e521a1ec21d163ef55b69fab65171572c4f9ff
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-07-05 23:55:24 +0300

    fixed emojis

--- src/Components/EmojiList/EmojiList.tsx
+++ src/Components/EmojiList/EmojiList.tsx
@@ -1,10 +1,18 @@
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
+import { useState } from 'react';
 export default function EmojiList() {
+  const [selectedEmoji, setSelectedEmoji] = useState<string | null>(null);
+  const handleEmojiSelect = (emoji: any) => {
+    setSelectedEmoji(emoji.native); // `native` contains the emoji character
+  };
   return (
     <div className='container d-flex flex-column align-items-center'>
-      <h1 className='text-light mb-5'>Emoji Picker!</h1>
-      <Picker data={data} onEmojiSelect={console.log} />
+      <h1 className='text-light'>Emoji Picker!</h1>
+      {selectedEmoji && (
+        <span style={{ fontSize: '2rem' }}>{selectedEmoji}</span>
+      )}
+      <Picker data={data} onEmojiSelect={handleEmojiSelect} />
     </div>
   )
 }
